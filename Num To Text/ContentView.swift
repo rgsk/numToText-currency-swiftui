422f3c495d35641eb7c7b150af563d8706f4da8b
rgsk <guptarahul70322@gmail.com>
2022-10-25 19:34:30 +0530

    Initial Commit

---
+++ Num To Text/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Num To Text
+//
+//  Created by Rahul Gupta on 25/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Num To Text/Num_To_TextApp.swift
@@ -0,0 +1,17 @@
+//
+//  Num_To_TextApp.swift
+//  Num To Text
+//
+//  Created by Rahul Gupta on 25/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Num_To_TextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
